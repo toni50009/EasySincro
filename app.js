cf59f2bfa2f3b67fc5078ec004961b457f8e5e70
Antonio Zanon <antonio.zanon99@gmail.com>
2024-12-19 00:07:15 -0300

    Dia 2 de trabalho, remodelado está com bug no finalDaAula

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const classeFormSegundo = document.querySelector(".conteudo__segundo-cadastro");
 let numeroGrupos;
 let numeroAulas;
 let grupoAtual;
+let horariosAulas;
+let duracaoAulas;
+let tempoMinutos;
+let finalDaAula;
+let repeticoes;
+let valorRepeticoesFinal;
 let listaGrupos = ["Grupo1" , "Grupo2", "Grupo3","Grupo4"];
+let posicaoAula = [];
 
 
 function primeiroCadastro(e){
@@ -16,12 +23,37 @@ function primeiroCadastro(e){
 
 
 function segundoCadastro(){
-    document.querySelector('h1').textContent = "Definir horário de Início e duração das aulas das turmas do grupo 1 ";
+    document.querySelector('h1').textContent = "Definir horário de Início e duração das aulas dos grupos de turmas";
     classeFormPrimeiro.classList.add('invisivel');
     classeFormSegundo.classList.remove('invisivel');
 }
 
 
-function inputsSegundoCadastro(){
-    
+function horarioAula(e){
+    e.preventDefault();
+    horarioAula = document.getElementById("horariosAulas").value;
+    duracaoAulas = document.getElementById("duracaoAulas").value;
+    repeticoes = document.getElementById("repeticoes").value;
+    converterHoras(horarioAula);
+    finalDaAula = parseInt(Number(horario) + Number(duracao));
+    alert(finalDaAula);
+    repetePadraoAulas(finalDaAula,duracaoAulas,repeticoes);
+    alert(valorRepeticoesFinal);
+}
+
+
+function converterHoras(horario,duracao){
+    tempoHoras = parseInt(horario.split(":")[0] * 60);
+    tempoMinutos = parseInt(horario.split(":")[1]);
+    alert(tempoHoras);
+    alert(tempoMinutos);
+}
+
+function repetePadraoAulas(final,duracao,qtdrepete){
+    valorRepeticoesFinal = Number(final + (qtdrepete * duracao));
+    return valorRepeticoesFinal;
+}
+
+function criarHorarios(){
+    novaLinha = document.createElement("")
 }
